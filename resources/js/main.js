69a1760176051e3e44dd1e87ab0c9a24bedd569f
jiin <ji961118@gmail.com>
2024-03-29 17:31:58 +0900

    feat : type3(탭안에탭형식) 추가

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,17 @@
-const summary = $('.course_tab_btn');
+const courseSummary = $('.course_tab_btn');
 
-summary.on('click', function () {
+courseSummary.on('click', function () {
+  if ($(this).parent().is('[open]')) {
+    $(this).parent().attr('open');
+    console.log('if');
+  } else {
+    $(this).parent().siblings().removeAttr('open');
+    console.log('else');
+  }
+});
+
+const areaSummary = $('.area_tab_btn');
+areaSummary.on('click', function () {
   if ($(this).parent().is('[open]')) {
     $(this).parent().attr('open');
     console.log('if');
